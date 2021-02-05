ad8747b0c2e75a1586c956f3b91ca0288a0adde5
Faith1sGay <daniel.viola001@mymdc.net>
2021-02-05 13:50:01 -0500

    Test bottum

---
+++ Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class Data
+{
+    ulong _id;
+    string _username;
+    DateTime _date;
+    public ulong Id
+    {
+        get
+        {
+            return _id;
+        }
+        set
+        {
+            _id = value;
+        }
+    }
+    public string UserName
+    {
+        get
+        {
+            return _username;
+        }
+        set
+        {
+            _username = value;
+        }
+    }
+    public DateTime Date
+    {
+        get
+        {
+            return _date;
+        }
+        set
+        {
+            _date = value;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using LiteDB;
+using Serilog.Sinks.SystemConsole;
+using DSharpPlus;
+using System.Threading.Tasks;
+namespace DatabaseTest
+{
+    class Program
+    {
+        static int Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+
+            async Task MainAsync()
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+
+
+                var logFactory = new LoggerFactory();
+                DiscordClient c = new DiscordClient(new DiscordConfiguration
+                {
+                    LoggerFactory = logFactory,
+                    MinimumLogLevel = LogLevel.Information,
+                    Token = "token",
+                    TokenType = TokenType.Bot
+                });
+                c.MessageCreated += async (s, e) =>
+                {
+                    if (e.Message.Content.ToLower().StartsWith("test"))
+                    {
+
+                        var Db = new LiteDatabase("./data.db");
+                        var col = Db.GetCollection<Data>("datatest");
+                        var d = new Data()
+                        {
+                            Id = e.Author.Id,
+                            UserName = e.Author.Username,
+                            Date = DateTime.UtcNow
+                        };
+                        try
+                        {
+                            col.Insert(d);
+                            await e.Message.RespondAsync("inserted");
+                        }
+                        catch (Exception E)
+                        {
+                            await e.Message.RespondAsync(E.ToString());
+                        }
+                    };
+                };
+                await c.ConnectAsync();
+                await Task.Delay(-1);
+            }
+            return 0;
+        }
+
+    }
+}
